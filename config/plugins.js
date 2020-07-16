a24afdb40fd70d2dd93e3fb0323984a055b9d432
F4brice74 <fabrice.event.manager@gmail.com>
2020-07-16 11:57:09 +0200

    made article and add cloudinary plugin and graphql

---
+++ config/plugins.js
@@ -0,0 +1,24 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET')
+    }
+  },
+  // ...
+});
+
+module.exports = {
+  //
+  graphql: {
+    endpoint: '/graphql',
+    tracing: false,
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 7,
+    amountLimit: 100,
+  },
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7a344e7d-24a0-4948-9b44-29b54d0ba6d7'
+};
