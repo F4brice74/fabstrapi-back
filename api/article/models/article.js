4f4a143284573d2c02fcf7ef4466fe7a96153188
F4brice74 <fabrice.event.manager@gmail.com>
2020-07-29 16:00:34 +0200

    mongoose connection

--- api/article/models/article.js
+++ api/article/models/article.js
@@ -1,25 +1,14 @@
-'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
 const slugify = require('slugify');
+
 module.exports = {
- /**
-   * Triggered before user creation.
-   */
   lifecycles: {
-    async beforeCreate(data) {
+    beforeCreate: async (data) => {
       if (data.title) {
-        data.slug = slugify(data.title, {lower: true});
+        data.slug = slugify(data.title);
       }
     },
-    async beforeUpdate(params, data) {
-      if (data.title) {
-        data.slug = slugify(data.title, {lower: true});
-      }
+    beforeUpdate: async (params, data) => {
+      data.slug = slugify(data.title);
     },
   },
-
 };
