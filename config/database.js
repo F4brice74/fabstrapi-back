2c4ac46971acd3216087dbb73b84369634a4f6cf
F4brice74 <fabrice.event.manager@gmail.com>
2020-07-29 17:17:19 +0200

    database ok

--- config/database.js
+++ config/database.js
@@ -4,7 +4,8 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI'),
+        uri:"mongodb://adminFab:MIldqxmwG3UAarzU@bdd-fab-shard-00-00.mfbyu.mongodb.net:27017,bdd-fab-shard-00-01.mfbyu.mongodb.net:27017,bdd-fab-shard-00-02.mfbyu.mongodb.net:27017/bdd-fab?ssl=true&replicaSet=atlas-kobq6i-shard-0&authSource=admin&retryWrites=true&w=majority"
+       // uri: env('DATABASE_URI'),
       },
       options: {
         ssl: true,
