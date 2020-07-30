1ffa07af4724b36031d4ea9fbb4a4ba724d62d9d
F4brice74 <fabrice.event.manager@gmail.com>
2020-07-30 12:14:13 +0200

    add slug to references

--- api/reference/models/reference.js
+++ api/reference/models/reference.js
@@ -1,8 +1,14 @@
-'use strict';
+const slugify = require('slugify');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.title) {
+        data.slug = slugify(data.title);
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      data.slug = slugify(data.title);
+    },
+  },
+};
