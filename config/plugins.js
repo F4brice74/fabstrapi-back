04c671ec79b226689cf4d8989be2a4c57615b62e
F4brice74 <fabrice.event.manager@gmail.com>
2020-08-27 15:52:18 +0200

    change env cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'cloudinary',
     providerOptions: {
-      cloud_name: env('CLOUDINARY_NAME'),
-      api_key: env('CLOUDINARY_KEY'),
-      api_secret: env('CLOUDINARY_SECRET'),
+      cloud_name: 'fabricework',
+      api_key: '256863963365415',
+      api_secret: 'CGKymO23mSrDelOqmOkhzuOWqyc',
     },
   },
   // ...
